f67d951b70541d27319c2fa68dcfd39c3decf76f
Jotavtech <vitinhucfelix@gmail.com>
2025-05-21 20:02:23 -0300

    vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig(async () => ({
-  base: process.env.VITE_BASE_PATH ||"/portfolio-final/",
+  base: process.env.VITE_BASE_PATH ||"/portfolioprofissional/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
