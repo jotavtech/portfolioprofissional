02f10814f3aa41dfe0b385262fb3383091a30e69
Jotavtech <vitinhucfelix@gmail.com>
2025-05-21 19:46:05 -0300

    correçao vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,18 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
-export default defineConfig({
+export default defineConfig(async () => ({
+  base: "/portfolio-final/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
     ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
+      process.env.REPL_ID !== undefined
       ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
-          ),
-        ]
+        (await import("@replit/vite-plugin-cartographer")).cartographer(),
+      ]
       : []),
   ],
-  base: "portfolio-final",
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
@@ -29,4 +27,4 @@ export default defineConfig({
     outDir: path.resolve(import.meta.dirname, "dist/public"),
     emptyOutDir: true,
   },
-});
+}));
