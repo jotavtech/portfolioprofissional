556413dd483cb192ccf9632a7937268e1875e279
Jotavtech <vitinhucfelix@gmail.com>
2025-05-21 21:21:19 -0300

    atualizacoes

--- vite.config.ts
+++ vite.config.ts
@@ -2,29 +2,43 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
-export default defineConfig(async () => ({
-  base: process.env.VITE_BASE_PATH ||"/portfolioprofissional/",
-  plugins: [
+// Use função assíncrona se for usar `await`
+export default defineConfig(async () => {
+  const plugins = [
     react(),
     runtimeErrorOverlay(),
-    ...(process.env.NODE_ENV !== "production" &&
-      process.env.REPL_ID !== undefined
-      ? [
-        (await import("@replit/vite-plugin-cartographer")).cartographer(),
-      ]
-      : []),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(import.meta.dirname, "client", "src"),
-      "@shared": path.resolve(import.meta.dirname, "shared"),
-      "@assets": path.resolve(import.meta.dirname, "attached_assets"),
+    viteStaticCopy({
+      targets: [
+        {
+          src: path.resolve(__dirname, "client/public/404.html"),
+          dest: "",
+        },
+      ],
+    }),
+  ];
+
+  // Adiciona plugin do Replit em modo dev
+  if (process.env.NODE_ENV !== "production" && process.env.REPL_ID !== undefined) {
+    const { cartographer } = await import("@replit/vite-plugin-cartographer");
+    plugins.push(cartographer());
+  }
+
+  return {
+    base: "/portfolioprofissional/",
+    plugins,
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "client", "src"),
+        "@shared": path.resolve(__dirname, "shared"),
+        "@assets": path.resolve(__dirname, "attached_assets"),
+      },
     },
-  },
-  root: path.resolve(import.meta.dirname, "client"),
-  build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
-    emptyOutDir: true,
-  },
-}));
+    root: path.resolve(__dirname, "client"),
+    build: {
+      outDir: path.resolve(__dirname, "dist/public"),
+      emptyOutDir: true,
+    },
+  };
+});
